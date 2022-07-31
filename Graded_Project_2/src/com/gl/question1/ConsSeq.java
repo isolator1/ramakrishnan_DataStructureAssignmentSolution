e99957eb92beb4975805c13a04d78e95dcfb7d9c
ramakrishnan <102976005+isolator1@users.noreply.github.com>
2022-07-31 23:40:59 +0530

    Add files via upload

---
+++ Graded_Project_2/src/com/gl/question1/ConsSeq.java
@@ -0,0 +1,56 @@
+package com.gl.question1;
+
+import java.util.*;
+
+class MyComparator implements Comparator
+
+{
+
+	public int compare(Object obj1, Object obj2)
+
+	{
+
+		Integer i1 = (Integer) obj1;
+
+		Integer i2 = (Integer) obj2;
+
+		return i2.compareTo(i1);
+
+	}
+
+}
+
+public class ConsSeq
+
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter No. of Floors of a Building :");
+
+		int n = sc.nextInt();
+
+		int max = n;
+
+		Queue<Integer> queue = new PriorityQueue<Integer>(new MyComparator());
+
+		int floorSize[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("");
+			System.out.println("Enter floor size on a given day : " + (1 + i) );
+
+			floorSize[i] = sc.nextInt();
+
+			queue.add(floorSize[i]);
+
+			while (!queue.isEmpty() && queue.peek() == max) {
+	
+				System.out.print("Construction Sequence for given day " + (1 + i) + " = " + (queue.poll()));
+				
+
+				max--;
+
+			}
+		}
+	}
+}
---
+++ Graded_Project_2/src/com/gl/question2/BST.java
@@ -0,0 +1,63 @@
+package com.gl.question2;
+
+class Node {
+	int val;
+	Node left, right;
+
+	
+	Node(int item) {
+		val = item;
+		left = right = null;
+	}
+}
+
+class BST {
+	public static Node node;
+	static Node prevNode = null;
+	static Node headNode = null;
+
+	
+	static void flattenBTToSkewed(Node root) {
+		if (root == null) {
+			return;
+		}
+		flattenBTToSkewed(root.left);
+		Node rightNode = root.right;
+		if (headNode == null) {
+			headNode = root;
+			root.left = null;
+			prevNode = root;
+		} else {
+			prevNode.right = root;
+			root.left = null;
+			prevNode = root;
+		}
+			flattenBTToSkewed(rightNode);
+
+	}
+
+	
+	static void traverseRightSkewed(Node root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.val + " ");
+		traverseRightSkewed(root.right);
+	}
+
+	public class Main {
+		
+		public static void main(String[] args) {
+		
+			
+			BST.node = new Node(50);
+			BST.node.left = new Node(30);
+			BST.node.right = new Node(60);
+			BST.node.left.left = new Node(10);
+			BST.node.right.left = new Node(55);
+	
+			flattenBTToSkewed(node);
+			traverseRightSkewed(headNode);
+		}
+	}
+}
